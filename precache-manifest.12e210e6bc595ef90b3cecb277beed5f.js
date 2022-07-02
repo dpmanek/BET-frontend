76d020300b606ebca2443ad2a1c22fddc9c27672
dpmanek <dpmanek@gmail.com>
2022-07-02 18:02:11 -0400

    Updates

---
+++ precache-manifest.12e210e6bc595ef90b3cecb277beed5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36da7b5ce39c9ab9cb08",
+    "url": "/static/css/main.d0d71590.chunk.css"
+  },
+  {
+    "revision": "36da7b5ce39c9ab9cb08",
+    "url": "/static/js/main.36da7b5c.chunk.js"
+  },
+  {
+    "revision": "240f4daddc7ec9cd9fc7",
+    "url": "/static/js/1.240f4dad.chunk.js"
+  },
+  {
+    "revision": "86316e3b353d11345b64",
+    "url": "/static/css/2.8b60d5e6.chunk.css"
+  },
+  {
+    "revision": "86316e3b353d11345b64",
+    "url": "/static/js/2.86316e3b.chunk.js"
+  },
+  {
+    "revision": "1adfb342440067e77d9b",
+    "url": "/static/js/runtime~main.1adfb342.js"
+  },
+  {
+    "revision": "36285be37579ebef8cee8b38675e679f",
+    "url": "/index.html"
+  }
+];
